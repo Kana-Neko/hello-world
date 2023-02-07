5120ef9a9a3b084094de0ff48cd24462a4099d33
Kanako Matsumoto <kanako.matsumoto1@pcc.edu>
2023-02-07 00:00:01 -0800

    added author and version

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,6 +1,11 @@
 package edu.pcc.cis.helloworld;
 
 import java.util.Date;
+
+/**
+ * @author Kanako Matsumoto
+ * @version 2023.02.06
+ */
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, world!");
